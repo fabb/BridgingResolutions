edd9f54e3edf6219a347bd45e46f5b62f12350df
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2017-09-06 08:21:44 +0200

    added comments describing the import problems

--- BridgingResolutions/BridgingResolutions-Bridging-Header.h
+++ BridgingResolutions/BridgingResolutions-Bridging-Header.h
@@ -2,5 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// the problem is that FBSDKCoreKit.h imports FBSDKAppEvents.h too, but with a FBSDKCoreKit/ prefix
 #import "FBSDKAppEvents.h"
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import "FBSDKCoreKit/FBSDKCoreKit.h"
+// or this, does not matter
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
